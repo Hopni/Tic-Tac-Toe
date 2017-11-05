e38580ad6dcd48272daa0c6cb7c5908f5dfd51cf
Hopni <anton.lugovoy@bk.ru>
2017-11-05 13:04:06 +0300

    Add files via upload

---
+++ src/tic-tac-toe.js
@@ -0,0 +1,116 @@
+class TicTacToe {
+    constructor() {
+        this.playerSymbol = 'x';
+        this.counter = 0;
+        this. field = [3];
+        for(var i = 0; i < 3; i++)
+        {
+            this.field[i] = [];
+        }
+        for(var i = 0; i < 3; i++)
+        {
+            for(var j = 0; j < 3; j++)
+            {
+                this.field[i][j] = null;
+            }
+        }
+    }
+
+    getCurrentPlayerSymbol() {
+        return this.playerSymbol;
+    }
+
+    nextTurn(rowIndex, columnIndex) {
+        if(this.field[rowIndex][columnIndex] === null)
+        {
+        this.counter++;
+        this.field[rowIndex][columnIndex] = this.playerSymbol;
+        if(this.playerSymbol === 'x')
+        {
+            this.playerSymbol = 'o';
+        }
+        else
+        {
+            this.playerSymbol = 'x';
+        }
+    }
+}
+
+    isFinished() {
+        if(this.noMoreTurns() || (this.getWinner() !== null))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    getWinner() {
+        for(var i = 0; i < 3; i++)
+        {
+            if((this.field[i][0] === this.field[i][1]) && (this.field[i][0] === this.field[i][2]) && (this.field[i][0] !== null))
+            {
+                return this.field[i][0];
+            }
+            if((this.field[0][i] === this.field[1][i]) && (this.field[0][i] === this.field[2][i]) && (this.field[0][i] !== null))
+            {
+                return this.field[0][i];
+            }
+            
+        }
+        if((this.field[0][0] === this.field[1][1]) && (this.field[0][0] === this.field[2][2]) && (this.field[0][0] !== null))
+        {
+            return this.field[0][0];
+        }
+        if((this.field[0][2] === this.field[1][1]) && (this.field[0][2] === this.field[2][2]) && (this.field[0][2] !== null))
+        {
+            return this.field[0][2];
+        }
+        return null;
+    }
+
+    noMoreTurns() {
+        if(this.counter >= 9)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    isDraw() {
+        if(this.noMoreTurns() && (this.getWinner() === null))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    getFieldValue(rowIndex, colIndex) {
+        return this.field[rowIndex][colIndex];
+    }
+}
+
+//module.exports = TicTacToe;
+var game = new TicTacToe();
+game.nextTurn(0, 0);
+game.nextTurn(0, 1)
+var a = game.getFieldValue(0, 2);
+a = game.getFieldValue(1, 2);
+a = game.getWinner();
+game.nextTurn(1, 0);
+game.nextTurn(1, 1);
+game.nextTurn(2, 2);
+game.nextTurn(2, 0);
+game.nextTurn(2, 1);
+game.nextTurn(0, 2);
+game.nextTurn(1, 2);
+a=game.getWinner();
+a = game.isDraw();
